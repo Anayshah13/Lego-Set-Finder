6de1c5898574c242396c8e71d08d64082f16783c
Anay Shah <anayshah10@gmail.com>
2025-07-04 12:58:59 +0530

    added sound

--- src/Animation.jsx
+++ src/Animation.jsx
@@ -1,12 +1,25 @@
+import { useEffect, useRef } from 'react';
+import './styles.css';
 
 function Animation() {
+  const audioRef = useRef(null);
+
+  useEffect(() => {
+    const audio = audioRef.current;
+    if (audio) {
+      audio.currentTime = 0.8;
+      audio.play().catch((e) => {
+        console.warn('Autoplay failed:', e);
+      });
+    }
+  }, []);
+
   return (
-    <div className="page">
-      <div className="c">
-        <img
-          src="/loading.gif"
-          alt="Loading animation"
-          style={{
+    <div className="loading-animation">
+      <img
+        src="/loading.gif"
+        alt="Loading animation"
+        style={{
             width: '60%',
             height: '60%',
             margin: '0 auto',
@@ -14,8 +27,8 @@ function Animation() {
             backgroundSize: 'cover',
             backgroundRepeat: 'noRepeat'
           }}
-        />
-      </div>
+      />
+      <audio ref={audioRef} src="/brick_placing.mp3" preload="auto" />
     </div>
   );
 }
